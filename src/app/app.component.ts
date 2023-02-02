05fad3d44a5bc5740099aea1725007af417a1256
naomi <naomi@miniasp.com>
2023-02-02 12:10:02 +0800

    (feat) add csv demo button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'csv-demo-root',
+  selector: 'dm2023-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { NxWelcomeComponent } from './nx-welcome.component';
+import { CsvDemoComponent } from './csv-demo/csv-demo.component';
 
 @NgModule({
-  declarations: [AppComponent, NxWelcomeComponent],
-  imports: [BrowserModule],
+  declarations: [AppComponent],
+  imports: [BrowserModule, CsvDemoComponent],
   providers: [],
   bootstrap: [AppComponent],
 })
