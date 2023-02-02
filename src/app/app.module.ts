99f9572b2c2f919320de348f3d3afd4611fdd549
kiwijang <kiwijang5473@gmail.com>
2023-02-02 16:03:08 +0800

    (fix) base href

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { APP_BASE_HREF } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
@@ -7,7 +8,7 @@ import { CsvDemoComponent } from './csv-demo/csv-demo.component';
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, CsvDemoComponent],
-  providers: [],
   bootstrap: [AppComponent],
+  providers: [{provide: APP_BASE_HREF, useValue: '/csvDemo/'}],
 })
 export class AppModule {}
